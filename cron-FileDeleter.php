afa79c570a1538558b0bb89c9482c0457f244db4
John Wellesz <wej@mikrosimage.eu>
2011-09-16 17:02:23 +0200

    WIP

--- cron-FileDeleter.php
+++ cron-FileDeleter.php
@@ -2,20 +2,64 @@
 // (c) John Wellesz for MikrosImage - September 2011
   
 
-error_reporting (E_ALL | E_STRICT );
+error_reporting ( E_ALL | E_STRICT );
 
 const DEFAULT_CONFIG_FILE = "cron-FileDeleter-paths.txt";
 const ERRORSTR = "ERROR: ";
 
-function printUsage ()
+function cprint ()
 {
-    echo "\nUsage: ", $_SERVER['PHP_SELF'], " [--config configFilePath] --remove | --show\n";
+    $args = func_get_args();
+
+    fwrite(STDOUT, implode($args, "")."\n");
 }
 
-function checkDirName ($name)
+function printUsage ()
+{
+    cprint ( "\nUsage: ", $_SERVER['PHP_SELF'], " [--config configFilePath] --remove | --show\n");
+}
+
+function error ()
+{
+    $args = func_get_args();
+
+    fwrite(STDERR, implode($args, "")."\n");
+}
+
+function errorExit($code)
+{
+    $args = func_get_args();
+    unset($args[0]);
+
+    fwrite(STDERR, implode($args, "")."\n");
+    exit ($code);
+}
+
+function isDirValid ($name)
 {
     // format should be something like BLABLABLA__TO_KEEP_XX_LENGTH
     // handle handle the following length : YEAR(S), MONTH(S), DAY(S), HOUR(S), MINUTE(S)
+
+    $matches = array();
+    $badDir = false;
+    $nameFormat = '#[/\\\\](\w+)__TO_KEEP_(\d+)_(YEAR|MONTH|DAY|HOUR|MINUTE|SECOND)S?$#';
+
+    if ( preg_match($nameFormat, $name, $matches) ) {
+	if ( is_dir ($name)) {
+
+	    var_dump($matches);
+
+	    return true;
+
+	} else 
+	    $badDir = "Directory '$name' cannot be found!";
+    } else
+	$badDir = "Wrong name format, should match '$nameFormat'";
+
+    if ($badDir) {
+	error('Config WARNING: ', "'$name'", " is not a valid directory:\n", $badDir);
+	return false;
+    }
 }
 
 function checkOptions ()
@@ -41,13 +85,11 @@ function checkOptions ()
 	define ('REMOVE', false);
 
 
-    if (SHOW && REMOVE) {
-	echo ERRORSTR,'--remove and --show options are exclusive!';
-	exit (1);
-    } elseif (! (SHOW || REMOVE)) {
-	echo ERRORSTR,"Action is missing!\n";
+    if (SHOW && REMOVE)
+	errorExit(1, ERRORSTR,'--remove and --show options are exclusive!');
+    elseif (! (SHOW || REMOVE)) {
 	printUsage ();
-	exit (1);
+	errorExit(1, ERRORSTR,"Action is missing!");
     }
 
     if (! empty($setOptions['c']) || ! empty($setOptions['config'])) {
@@ -55,10 +97,8 @@ function checkOptions ()
 
 	if ( file_exists( $config ) )
 	    define ('CONFIG', realpath($config));
-	else {
-	    echo ERRORSTR,"config file '$config' couldn't be found!";
-	    exit (1);
-	}
+	else
+	    errorExit(1, ERRORSTR,"config file '$config' couldn't be found!");
     } else
 	define ('CONFIG', false);
 }
@@ -72,26 +112,21 @@ function getConfig ()
 
 	if (file_exists($configPath))
 	    $config = file($configPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	else {
-	    echo ERRORSTR,'No configuration file provided and no file named "', DEFAULT_CONFIG_FILE, '" found in ', getcwd(); 
-	    exit (1);
-	}
+	else
+	    errorExit(1, ERRORSTR,'No configuration file provided and no file named "', DEFAULT_CONFIG_FILE, '" found in ', getcwd());
     }
 
-    if (count($config) == 0 ) {
-	echo ERRORSTR,'Configuration file is empty!', ' Configuration file used : ', ( isset($configPath) ? realpath($configPath) : CONFIG );
-	exit (2);
-    }
+    if (count($config) == 0 )
+	errorExit(2, ERRORSTR,'Configuration file is empty!', ' Configuration file used : ', ( isset($configPath) ? realpath($configPath) : CONFIG ));
 
     foreach ($config as $path)
-	if (! is_dir($path)) {
-	    echo 'Config WARNING: ', "'$path'", " is not a valid directory.\n";
+	if (! isDirValid($path)) {
 	    unset ($config[$path]);
 	}
 
 }
 
-echo "\nHello fucking world!\n\n";
+cprint ("\nHello fucking world!\n");
 
 checkOptions ();
 getConfig ();
@@ -103,7 +138,7 @@ getConfig ();
 
 
 
-echo "\n";
+cprint ("");
 
 /*  TODO :
  *
