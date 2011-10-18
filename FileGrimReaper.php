ebb8aaddad573f8db5c9449cdbc9c4154e86e69c
John Wellesz <wej@mikrosimage.eu>
2011-10-18 18:15:42 +0200

    - Include GPL license

--- FileGrimReaper.php
+++ FileGrimReaper.php
@@ -1,5 +1,24 @@
 <?php
 
+/* File Grim Reaper - It will reap your files!
+ * (c) 2011 John Wellesz
+ *
+ *   This file is part of File Grim Reaper.
+ *
+ *   File Grim Reaper is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   File Grim Reaper is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with File Grim Reaper. If not, see <http://www.gnu.org/licenses/>.*   
+ */
+
 const PROPER_USAGE = true;
 
 if (! version_compare(PHP_VERSION, '5.3.8', '>='))
--- core.php
+++ core.php
@@ -1,7 +1,23 @@
 <?php
-// (c) John Wellesz for MikrosImage - September 2011
 
-//php FileGrimReaper.php --show -c=testConfig.txt --dry-run
+/* File Grim Reaper - It will reap your files!
+ * (c) 2011 John Wellesz
+ *   
+ *   This file is part of File Grim Reaper.
+ *
+ *   File Grim Reaper is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   File Grim Reaper is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with File Grim Reaper. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 const VERSION = 1;
 const REVISION = 0;
